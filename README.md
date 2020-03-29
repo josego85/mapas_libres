601ab40cd6ec6a619ef5b3185193f4a7b142186d
Jose Alberto González von Schmeling <josego85@gmail.com>
2020-03-29 12:42:07 -0400

    Se agrega mas ejemplos del live en twitch.tv

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ El codigo esta basado en lo que sale del live de Twitch. Consiste en mostrar con
 
 Desarrollo:
 - Librería de mapas: Leaflet (https://leafletjs.com/)
-- Lenguajes de programaciósn: HTML, CSS y JavaScript
+- Lenguajes de programación: HTML, CSS y JavaScript
 
 Todo list:
-- Control de layers. Layer base con layers secundarios.
 - Recuperar de un archivo GeoJSON usando Ajax.
+- Filtros, estilos GeoJSON.
+- Control de layers. Layer base con layers secundarios.
 - Buscador utilizando Nominatim de OSM.
 - Conactarse a una base de datos PostgreSQL/PostGIS, cargando y recuperando datos.
 - Agregar API pata el manejo de la carga y recuperacion de datos.
---
+++ public/assets/js/MapaOSM1.js
@@ -0,0 +1,148 @@
+var mapa = null;
+var geojsonLayer = null;
+var info = null;
+
+function cargarMapa()
+{
+	// Asuncion - Paraguay.
+	var longitud = -57.6309129;
+	var latitud = -23.2961407;
+	var zoom = 6;
+	var minZoom = 6;
+	var maxZoom = 18;
+
+	// Se instancia el objeto mapa.
+	mapa = new L.map('mapa',
+    {
+        center: [latitud, longitud],
+        minZoom: minZoom,
+		maxZoom: maxZoom,
+		zoom: zoom,
+        scrollWheelZoom: false,
+        fullscreenControl: true,
+        fullscreenControlOptions:
+        {
+            position: 'topleft'
+        }
+    });
+
+	// Humanitarian Style.
+	var url = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+	var layerOSM = L.tileLayer(url,
+	{
+		maxZoom: 18,
+		attribution: 'Data \u00a9 <a href="http://www.openstreetmap.org/copyright">' +
+          'OpenStreetMap Contributors </a> Tiles \u00a9 HOT'
+	}).addTo(mapa);
+
+	$.getJSON("assets/data/Departamentos.geojson", function(data)
+	{
+		geojsonLayer = L.geoJson(data,
+		{
+			style: style,
+			onEachFeature: onEachFeature,
+			filter: function(p_feature, p_layer)
+			{
+				var area = p_feature.properties.Area;
+				var minArea = 50000000000;
+				var maxArea = 90000000000;
+
+				return (parseInt(area) > minArea && parseInt(area) < maxArea);
+			}
+		});
+		mapa.addLayer(geojsonLayer);
+
+		var baseMaps =
+		{
+			"OSM": layerOSM
+		};
+		
+		var overlayMaps =
+		{
+			'Departamentos': geojsonLayer,
+		};
+		L.control.layers(baseMaps, overlayMaps,
+		{
+			//collapsed: false
+		}).addTo(mapa);
+		mostrarInfo();
+	});
+}
+
+function mostrarInfo ()
+{
+	info = L.control();
+
+	info.onAdd = function (map)
+	{
+		this._div = L.DomUtil.create('div', 'info');
+		this.update();
+		return this._div;
+	};
+
+	info.update = function (props)
+	{
+		this._div.innerHTML = '<h4>&Aacute;rea departamento</h4>' +  (props ?
+			'<b>' + props.Nombre + '</b><br />' + props.Area + ' m<sup>2</sup>'
+			: 'Pase el ratón sobre un departamento');
+	};
+
+	info.addTo(mapa);
+}
+
+// Funcion que muestra informacion en un popup.
+function onEachFeature(p_feature, p_layer)
+{
+	p_layer.on(
+	{
+		mouseover: highlightFeature,
+		mouseout: resetHighlight
+	});
+}
+
+function style (feature)
+{
+	var area = parseInt(feature.properties.Area);
+
+	return {
+		fillColor: getColor(area),
+		weight: 2,
+		opacity: 1,
+		color: 'white',
+		dashArray: '3',
+		fillOpacity: 0.7
+	};
+}
+
+function getColor (area)
+{
+	return area > 90000000000 ? '#800026' :
+	   area > 80000000000 ? '#BD0026' :
+	   area > 40000000000 ? '#E31A1C' :
+	   area > 20000000000 ? '#FC4E2A' :
+	   area > 15000000000 ? '#FD8D3C' :
+	   area > 12000000000 ? '#FEB24C' :
+	   area > 10000000000 ? '#FED976' :
+		'#FED976';
+}
+
+function highlightFeature(e)
+{
+	var layer = e.target;
+
+	layer.setStyle(
+	{
+		weight: 5,
+		color: '#666',
+		dashArray: '',
+		fillOpacity: 0.7
+	});
+	layer.bringToFront();
+	info.update(layer.feature.properties);
+}
+
+function resetHighlight(e)
+{
+	geojsonLayer.resetStyle(e.target);
+	info.update();
+}
